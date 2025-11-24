37837b2d2e304234e383ddb366ce27d9a3282c61
FlaviaBehrend <flahbehrend@gmail.com>
2025-11-24 17:08:32 -0300

    ghjkgg

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
 document.getElementById("formcadastro").addEventListener("submit", function(event)/*quando clicar o botao, o event é um objeto com informação de eventos que é enviado pelo navegador*/{/*Submeter dados e não carregar outra pág */ 
     event.preventDefault();
+    var nome = document.getElementById("nome").value;
+    var idade = document.getElementById("idade").value;
 })
