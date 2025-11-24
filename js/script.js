5fd0adfaa90a3b78c5fcd27b7bc4596f34c14fd0
FlaviaBehrend <flahbehrend@gmail.com>
2025-11-24 17:06:17 -0300

    funcao

---
+++ js/script.js
@@ -0,0 +1,3 @@
+document.getElementById("formcadastro").addEventListener("submit", function(event)/*quando clicar o botao, o event é um objeto com informação de eventos que é enviado pelo navegador*/{/*Submeter dados e não carregar outra pág */ 
+    event.preventDefault();
+})
